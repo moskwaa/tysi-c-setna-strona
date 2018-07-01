85ac0c12957e9a4b780523651e7cde6591a29dfc
moskwaa <moskwa.anna@gmail.com>
2018-07-01 14:31:14 +0200

    gallery js

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+    $(document).ready(function () {
+        console.log('use');
+        $('.gallery-image').slick({
+            dots: true,
+            arrows: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                    arrows: true
+                }
+                    }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+                    }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                    }]
+        });
+    });
